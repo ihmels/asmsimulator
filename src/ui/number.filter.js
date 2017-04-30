ea6414105b96fa7f144c2bbeb33d136bbdb245eb
Yannick Ihmels <yannick@ihmels.com>
2017-04-30 17:22:02 +0200

    Change number filter to allow different width of hex values

--- src/ui/number.filter.js
+++ src/ui/number.filter.js
@@ -1,8 +1,18 @@
 app.filter('number', function() {
-    return function(input, isHex) {
+    return function(input, isHex, width) {
         if (isHex) {
             var hex = input.toString(16).toUpperCase();
-            return hex.length == 1 ? '0' + hex: hex;
+            var diff = width - hex.length;
+            
+            if (!width | width < 0 || diff < 0)
+                return hex;
+                
+            while (diff > 0) {
+                hex = '0' + hex;
+                diff--;
+            }
+            
+            return hex;
         } else {
             return input.toString(10);
         }
