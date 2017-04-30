b1c65ab99d0ffc29fed14e93e715d0672fc3a7a3
Yannick Ihmels <yannick@ihmels.com>
2017-04-30 11:27:50 +0200

    Switch back to ES5

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,32 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/app.js', 'src/**/*.js'],
+                dest: 'app/<%= pkg.name %>.js'
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js']
+        },
+        uglify: {
+          options: {
+            mangle: false
+          },
+          dist: {
+            files: {
+              'app/asmsimulator.js': ['app/asmsimulator.js']
+            }
+          }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'concat']
+        },
         connect: {
           server: {
             options: {
@@ -10,34 +36,15 @@ module.exports = function(grunt) {
               keepalive: true
             }
           }
-        },
-        concat: {
-            options: {
-                separator: ';\n'
-            },
-            dist: {
-                src: ['src/app.js', 'src/**/*.js'],
-                dest: 'app/<%= pkg.name %>.js'
-            }
-        },
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js'],
-            options: {
-                esversion: 6
-            }
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'concat']
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'connect']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
