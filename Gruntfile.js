f894809944da36367840978e6391d2890c5aa7f9
Yannick Ihmels <yannick@ihmels.org>
2018-01-01 15:31:09 +0100

    Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,42 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js']
-        },
-        uglify: {
-          options: {
-            mangle: false
-          },
-          dist: {
-            files: {
-              'app/asmsimulator.js': ['src/app.js', 'src/**/*.js']
-            }
-          }
-        },
-        copy: {
-          js_angularjs: {
-            src: 'node_modules/angular/angular.min.js',
-            dest: 'app/angular.js'
-          },
-          js_bootstrap: {
-            src: 'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            dest: 'app/bootstrap.js'
-          },
-          js_jquery: {
-            src: 'node_modules/jquery/dist/jquery.min.js',
-            dest: 'app/jquery.js'
-          },
-          css_bootstrap: {
-            src: 'node_modules/bootstrap/dist/css/bootstrap.min.css',
-            dest: 'app/bootstrap.css'
-          }
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'concat']
-        },
-        connect: {
-          server: {
-            options: {
-              port: 8080,
-              base: 'app',
-              keepalive: true
-            }
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      copy: {
+        dist: {
+          expand: true,
+          flatten: true,
+          src: [
+            'node_modules/angular/angular.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/css/bootstrap.min.css'
+          ],
+          dest: 'app/'
+        }
+      },
+      jshint: {
+        files: ['Gruntfile.js', 'src/**/*.js']
+      },
+      uglify: {
+        dist: {
+          files: {
+            'app/asmsimulator.js': ['src/app.js', 'src/**/*.js']
           }
         }
-    });
+      },
+      watch: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint', 'uglify']
+      }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jshint', 'copy', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'copy', 'uglify']);
 
 };
+
